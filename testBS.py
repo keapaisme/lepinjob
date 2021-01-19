a26883245febfd207247b3adaba4fc0523c51418
DESKTOP-7OPJ51K\kas <kaskaskas>
2021-01-20 03:15:48 +0800

    完成BS4用於拮取單一網頁資料

--- testBS.py
+++ testBS.py
@@ -4,16 +4,20 @@
 # @File:testBS.py
 # @Software:PyCharm
 
-
 from bs4 import BeautifulSoup
+
 '''
 這裡使用 encoding="utf-8" 否則BS報錯 BeautifulSoup 報錯UnicodeDecodeError
 '''
 listhtml = open("page.html", "r", encoding="utf-8")
 bs = BeautifulSoup(listhtml, "html.parser")
 
-resultList = bs.select(".list .e.e3")  # 選擇器,ID用#,class用.,往下層級用>
+result_link = bs.select(".list .e.e3")  # 選擇器,ID用#,class用.,往下層級用>
 
-for i in resultList:
-    print(i["href"])  # 這可能是BS4的語法
+for i in result_link:
+    # print(i['href'])  # 印出連結
+    # print(i.string)  # 只印出TAG裡的字串
+    print(i.em.string, i.aside.string, i.span.string, i.i.string, i['href'])  # 地區,取得公司名,職位,薪水,頁面連結
+
+
 
