8bcf15049f39a125f3146d60518a16fc9e22dce7
DESKTOP-7OPJ51K\kas <kaskaskas>
2021-01-20 05:08:00 +0800

    從testBS轉碼到spiderLk建立一个頁查詢

--- spiderLK.py
+++ spiderLK.py
@@ -14,17 +14,15 @@ import sqlite3
 
 # keytitle = input("請輸入要搜尋的工作 :")
 # keycity = input("請輸入要搜尋的地區 :") 090200成都,010000北京,040000深圳,000000所有
-keytitle = "洗衣" # 測試用,正常使用時改以上方方式輸入
-pageNum = 0
+keytitle = "洗衣"  # 測試用,正常使用時改以上方方式輸入
+pageNum = 1  # 51job 要用1, 猎聘用0
 kw = parse.quote(parse.quote(keytitle))  # 原網站將中文編譯兩次所以出現%25字節
 
 
-
 def main():
-
-    baseurl = "https://search.51job.com/list/090200,000000,0000,00,9,99," + kw + ",2," + str(pageNum) + ".html"
+    url = "https://search.51job.com/list/090200,000000,0000,00,9,99,"+ kw +",2,"+ str(pageNum) +".html"
     # baseurl = "https://www.liepin.com/zhaopin/?init=-1&fromSearchBtn=2&degradeFlag=0&key=" + parse.quote(keytitle) + "&d_sfrom=search_unknown&d_pageSize=40&curPage=%d"%pageNum  # 猎聘
-    datalist = getData(baseurl)
+    getData(url)
 
 
 def askURL(url):
@@ -43,19 +41,20 @@ def askURL(url):
     return html
 
 
-def getData(baseurl):
+def getData(url):
     datalist = []
-    for i in range(0, 1):
-        url = baseurl
-        # print(url)  # 測試有否正常捉取網頁
-        html = askURL(url)
-        # print(url)
+    html = askURL(url)
 
-        # 逐一解析數据
-        soup = BeautifulSoup(html, "html.parser")
-        print(soup)
-        for item in soup.find_all('div', class_=r"sojob-item-main clearfix"):
-            print(item)
+    # 逐一解析數据
+    bs = BeautifulSoup(html, "html.parser")
+    result_link = bs.select(".list .e.e3")  # 選擇器,ID用#,class用.,往下層級用>
+    x = 0
+    for i in result_link:
+        x += 1
+        # print(i['href'])  # 印出連結
+        # print(i.string)  # 只印出TAG裡的字串
+        # strlist = i.em.string + i.aside.string + i.span.string + i.i.string + i['href']
+        print(x, i.em.string, i.aside.string, i.span.string, i.i.string, i['href'])  # 地區,取得公司名,職位,薪水,頁面連結
 
     return datalist
 
@@ -68,6 +67,6 @@ def init_db(dbpath):
     pass
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
     print("爬取 over")
